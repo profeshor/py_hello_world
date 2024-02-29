fa4a491ae768de6832a99f604b9b195b3e786049
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2024-02-28 21:34:01 -0500

    agregar signo de admiración

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 def welcome_message():
     name = input ("Ingrese su nombre: ")
-    print("Bienvenido", name, ", al curso de Django y React")
+    print("Bienvenido", name, ", al curso de Django y React!")
 
 if __name__ == "__main__":
     welcome_message()
